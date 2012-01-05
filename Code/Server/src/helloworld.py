dfd108667437fae212747dacce7de8355719a777
Karl Wenzel <karl.wenzel@wenzeltech.com>
2012-01-05 13:31:54 -0600

    Adding models directory, which includes OilGasLease and Tract

--- Code/Server/src/helloworld.py
+++ Code/Server/src/helloworld.py
@@ -1,14 +1,31 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+from models.OilGasLease import OilGasLease
+from models.Tract import Tract
 
 class MainPage(webapp.RequestHandler):
     
     
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
-
+        
+        ogl = OilGasLease()
+        ogl.bookPage = '12/345'
+        ogl.county = 'Custer'
+        
+        t = Tract()
+        t.sec = '01'
+        t.twn = '02N'
+        t.rng = '03W'
+        
+        #ogl.put()
+        #t.oilGasLease = ogl
+        #t.put()
+        
+        outputString = 'a tract: ' + t.secTwnRng() + '\n' + 'a lease: ' + str(ogl)
+        
+        self.response.out.write(outputString)
 
 application = webapp.WSGIApplication([('/', MainPage)], debug=True)
 
